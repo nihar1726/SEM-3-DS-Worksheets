bc3658b6f18e4fa828a76fa90066da6b1b969f22
nihar1726 <pnihar2705@gmail.com>
2024-07-22 20:12:30 +0530

    Corrected multiplication of matrices!

--- Exp 1.2.cpp
+++ Exp 1.2.cpp
@@ -1,75 +1,74 @@
 // WAP to find sum and product of two matrices of 2x2 order
 
+// 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-   int rows, cols, i, j;
-   cout<<"Enter the number of rows:";
-   cin>>rows;
-   cout<<"Enter the number of columns:";
-   cin>>cols;
+int rows, cols, i, j ,k;
+cout<<"Enter rows:";
+cin>>rows;
+cout<<"Enter columns:";
+cin>>cols;
 
-   int a1[rows-1][cols-1], a2[rows-1][cols-1], a3[rows-1][cols-1], a4[rows-1][cols-1];
+int a1[rows-1][cols-1], a2[rows-1][cols-1], sum[i][j], product[i][j];
 
-   cout<<"Enter Elements in first 2D array:"<<endl;
-   for (i=0; i<=rows-1; i++)
-   {
-    for(j=0; j<=cols-1; j++)
+cout<<"Enter data into first matrix:"<<endl;
+for (i=0; i<=rows-1; i++)
+{
+    for (j=0; j<=cols-1; j++)
     {
         cin>>a1[i][j];
     }
-    cout<<endl;
-   }
-
-   cout<<"Enter Elements in second 2D array:"<<endl;
-   for (i=0; i<=rows-1; i++)
-   {
-    for(j=0; j<=cols-1; j++)
+}
+cout<<"Enter data into second matrix:"<<endl;
+for (i=0; i<=rows-1; i++)
+{
+    for (j=0; j<=cols-1; j++)
     {
         cin>>a2[i][j];
     }
-    cout<<endl;
-   }
+}
 
-   for(i=0; i<=rows-1; i++)
-   {
+for (i=0; i<=rows-1; i++)
+{
     for (j=0; j<=cols-1; j++)
     {
-        a3[i][j] = a1[i][j] + a2[i][j];
+        sum[i][j] = a1[i][j] + a2[i][j];
     }
-   }
-
-   cout<<"Sum of two 2D arrays:"<<endl;
-   for (i=0; i<=rows-1; i++)
-   {
-    for(j=0; j<=cols-1; j++)
-    {
-        cout<<" "<<a3[i][j];
-    }
-    cout<<endl;
-   }
-
-    for(i=0; i<=rows-1; i++)
-   {
+}
+cout<<"Addition of the two matrices:"<<endl;
+for (i=0; i<=rows-1; i++)
+{
     for (j=0; j<=cols-1; j++)
     {
-        a4[i][j] = a1[i][j] * a2[i][j];
-    }
-   }
-
-   cout<<"Product of two 2D arrays:"<<endl;
-   for (i=0; i<=rows-1; i++)
-   {
-    for(j=0; j<=cols-1; j++)
-    {
-        cout<<' '<<a4[i][j];
+       cout<<" "<<sum[i][j];
     }
     cout<<endl;
-   }
+}
 
+for (i=0; i<=rows-1; ++i)
+{
+    for (j=0; j<=cols-1; ++j)
+    {
+        product[i][j]=0;
+
+        for (k=0; k<=rows-1; ++k)
+        {
+            product[i][j] += a1[i][k] * a2[k][j];
+        }
+    }
+
+}
+cout<<"Product of the two matrices:"<<endl;
+for (i=0; i<=rows-1; i++)
+{
+    for (j=0; j<=cols-1; j++)
+    {
+       cout<<" "<<product[i][j];
+    }
+    cout<<endl;
+}
 return 0;
-   
-
 }
