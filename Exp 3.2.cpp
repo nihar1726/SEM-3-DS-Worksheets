496acfee66fd8fe50bcc6f279be695bca5544a1a
Nihar <158161940+nihar1726@users.noreply.github.com>
2024-11-10 13:11:05 +0530

    .

--- Exp 3.2.cpp
+++ Exp 3.2.cpp
@@ -1,39 +1,44 @@
-//searching an element using binary search
+// searching an element using binary search
 #include <iostream>
 using namespace std;
 
 int binary_search(int array[], int low, int high, int element)
 {
-    int mid = (low + high)/2;
 
     while (low <= high)
     {
+        int mid = (low + high) / 2;
+
         if (array[mid] == element)
             return mid;
         if (array[mid] > element)
             high = mid - 1;
-        else 
+        else // element is greater
             low = mid + 1;
     }
     return -1;
 }
 
-int main (void)
+int main(void)
 {
-    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    int n = sizeof (arr) / sizeof (arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int ele = 5;
+    int ele;
+    cout << "Enter element you wish to search: ";
+    cin >> ele;
+
     int result = binary_search(arr, 0, n - 1, ele);
 
     if (result == -1)
     {
-        cout<<"Element not present in array!";
+        cout << "Element not present in array!";
     }
     else
     {
-        cout<<"Position of element: "<<++result<<endl;
-    return 0;
+        cout << "Position of element: " << ++result << endl;
     }
+
+    return 0;
 }
--- Exp. 4.2.cpp
+++ Exp. 4.2.cpp
@@ -1,27 +1,27 @@
-1
+
 // Simple C++ program to add two polynomials
 #include <iostream>
 using namespace std;
 
 // A utility function to return maximum of two integers
-int max(int m, int n) 
-{ 
-    return (m > n) ? m : n; 
+int max(int m, int n)
+{
+    return (m > n) ? m : n;
 }
 
 // A[] represents coefficients of first polynomial
 // B[] represents coefficients of second polynomial
 // m and n are sizes of A[] and B[] respectively
-int* add(int expression1[], int expression2[], int size_of_exp1, int size_of_exp2)
+int *add(int expression1[], int expression2[], int size_of_exp1, int size_of_exp2)
 {
     int size = max(size_of_exp1, size_of_exp2);
-    int* sum = new int[size];
+    int *sum = new int[size];
 
-    // Initialize the product polynomial
+    // Copy first polynomial to sum array
     for (int i = 0; i < size_of_exp1; i++)
         sum[i] = expression1[i];
 
-    // Take every term of first polynomial
+    // Add second and first polynomial
     for (int i = 0; i < size_of_exp2; i++)
         sum[i] += expression2[i];
 
@@ -31,7 +31,8 @@ int* add(int expression1[], int expression2[], int size_of_exp1, int size_of_exp
 // A utility function to print a polynomial
 void printPoly(int poly[], int p)
 {
-    for (int i = 0; i < p; i++) {
+    for (int i = 0; i < p; i++)
+    {
         cout << poly[i];
         if (i != 0)
             cout << "x^" << i;
@@ -45,11 +46,11 @@ int main()
 {
     // The following array represents polynomial 5 + 10x^2 +
     // 6x^3
-    int exp1[] = { 5, 0, 10, 6 };
+    int exp1[] = {5, 0, 10, 6};
 
     // The following array represents polynomial 1 + 2x +
     // 4x^2
-    int exp2[] = { 1, 2, 4 };
+    int exp2[] = {1, 2, 4};
     int m = sizeof(exp1) / sizeof(exp1[0]);
     int n = sizeof(exp2) / sizeof(exp2[0]);
 
@@ -58,7 +59,7 @@ int main()
     cout << "\nSecond polynomial is \n";
     printPoly(exp2, n);
 
-    int* sum = add(exp1, exp2, m, n);
+    int *sum = add(exp1, exp2, m, n);
     int size = max(m, n);
 
     cout << "\nsum polynomial is \n";
